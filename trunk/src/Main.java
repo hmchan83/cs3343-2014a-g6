4658114914455d6a7dbca6aa8d3446b9f283ddf3
qooleeman@gmail.com <qooleeman@gmail.com@cabb5b36-2792-8a72-6769-e652e2edd7b8>
2014-09-25 17:13:08 +0000

    remove useless folder and file, modify some code and add comment

--- trunk/src/Main.java
+++ trunk/src/Main.java
@@ -3,7 +3,7 @@ import ioMoudle.*;
 import java.util.ArrayList;
 
 import Controller.MainController;
-import Controller.PriorityHandler;
+import Controller.ListHandler;
 import store.Course;
 import store.StoredItem;
 /**
@@ -20,7 +20,7 @@ public class Main {
 		ArrayList<Course> courselist = io.input(); //for testing just using simple input
 		courseNums = courselist.size();
 		
-		PriorityHandler PriorityHandler=new PriorityHandler();		
+		ListHandler PriorityHandler=new ListHandler();		
 		PriorityHandler.listformat(courselist);
 		
 		io.out("\nYour Input : \n");
@@ -33,9 +33,12 @@ public class Main {
 		MainController.run(courselist); // calling controller
 		ArrayList<StoredItem> result = MainController.result();
 		io.out("\nResult : \n");
+		int totalpriority=0;
 		for(StoredItem item : result){
 			io.out(item.toString());
+			totalpriority+=item.getSec().getPriority();
 		}
+		io.out("\nTotal Priority = "+totalpriority);
 			
 	}
 }
