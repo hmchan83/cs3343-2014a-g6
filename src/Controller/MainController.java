f501e08294fd40c5c9c758fc2113798dee6cc796
qooleeman@gmail.com <qooleeman@gmail.com@cabb5b36-2792-8a72-6769-e652e2edd7b8>
2014-10-03 16:44:37 +0000

    temp upload , nothing completed in the update

--- src/Controller/MainController.java
+++ src/Controller/MainController.java
@@ -23,32 +23,30 @@ public class MainController {
 	public void run(ArrayList<Course> list){
 		boolean flag=false;
 		DebugMessager.debug("MainController Start");
+		
+		if(SimpleHandler == true){ // No priory problem
 				
-		for(int i=0;i<list.size();i++){
-			flag=false; // course is not added
-			Course currCourse=list.get(i);
-			int minConflict = currCourse.getMinConflict();
-			//int SecMinConflictNum = currCourse.getSecNumMinConflict();
-			DebugMessager.debug("The min Conflict of this course = "+minConflict);
-			DebugMessager.debug("Handling Course "+ i+" {"+currCourse.toString()+"}");
-			
-			/*
-			 * if the section of course are having the same conflict number
-			 * we may try selecting it and find the highest priory result
-			 */
-			
-			
-			if( SimpleHandler == false ){/* priory handler */
+			for(int i=0;i<list.size();i++){
+				flag=false; // course is not added
+				Course currCourse=list.get(i);
+				int minConflict = currCourse.getMinConflict();
+				//int SecMinConflictNum = currCourse.getSecNumMinConflict();
+				DebugMessager.debug("The min Conflict of this course = "+minConflict);
+				DebugMessager.debug("Handling Course "+ i+" {"+currCourse.toString()+"}");
 				
-			}else{
+				/*
+				 * if the section of course are having the same conflict number
+				 * we may try selecting it and find the highest priory result
+				 */			
 				for(int j=0;j<currCourse.getSec().size();j++){
 					Section currSec = currCourse.getSec().get(j);
 					DebugMessager.debug("Handling Section "+ i+" {"+currSec.toString()+"}");
 					int conflictNums = ConflictDetector.run(list, i, currSec);
 					DebugMessager.debug("conflictNums = "+conflictNums);
 					if(conflictNums > list.size()/2 || (minConflict==0 && conflictNums!=minConflict)){
+						//Conflict with half of the course may not be a good option
 						DebugMessager.debug("Course Dropped");
-						continue; //bad option
+						continue; //bad option, select next session
 					}
 					if(selectCourse(currSec)==true){
 						StoredItem tempVal = new StoredItem(currCourse.getCourseID(),currCourse.getCourseName(),currSec);//Simple value
@@ -58,9 +56,12 @@ public class MainController {
 						break; // don't need to handle other section this time
 					}
 				}
-			}
 			
-			if(flag==true) continue; // skip to next course
+				if(flag==true) continue; // skip to next course
+			}
+		}else{
+			ComplexHandler ComplexHandler=new ComplexHandler();
+			ComplexHandler.run(list);
 		}
 	}
 	
