42d30aa958fcde11b103ca819facc149f0cc4fcd
qooleeman@gmail.com <qooleeman@gmail.com@cabb5b36-2792-8a72-6769-e652e2edd7b8>
2014-10-03 19:44:18 +0000

    Debug message optimize, brake things into 2 handler, ComplexHandler start develop. For more about the logic flow, read the wiki from googlecode

--- src/Controller/MainController.java
+++ src/Controller/MainController.java
@@ -15,53 +15,24 @@ import store.StoredItem;
  */
 
 public class MainController {
-	
+	static String title = "MainController : ";
 	static Boolean SimpleHandler;
 	private OverlapDetector table = new OverlapDetector();
 	private ArrayList<StoredItem> selected = new ArrayList<StoredItem>();
 	
 	public void run(ArrayList<Course> list){
-		boolean flag=false;
-		DebugMessager.debug("MainController Start");
+		DebugMessager.debug(title+"MainController Start");
 		
 		if(SimpleHandler == true){ // No priory problem
-				
-			for(int i=0;i<list.size();i++){
-				flag=false; // course is not added
-				Course currCourse=list.get(i);
-				int minConflict = currCourse.getMinConflict();
-				//int SecMinConflictNum = currCourse.getSecNumMinConflict();
-				DebugMessager.debug("The min Conflict of this course = "+minConflict);
-				DebugMessager.debug("Handling Course "+ i+" {"+currCourse.toString()+"}");
-				
-				/*
-				 * if the section of course are having the same conflict number
-				 * we may try selecting it and find the highest priory result
-				 */			
-				for(int j=0;j<currCourse.getSec().size();j++){
-					Section currSec = currCourse.getSec().get(j);
-					DebugMessager.debug("Handling Section "+ i+" {"+currSec.toString()+"}");
-					int conflictNums = ConflictDetector.run(list, i, currSec);
-					DebugMessager.debug("conflictNums = "+conflictNums);
-					if(conflictNums > list.size()/2 || (minConflict==0 && conflictNums!=minConflict)){
-						//Conflict with half of the course may not be a good option
-						DebugMessager.debug("Course Dropped");
-						continue; //bad option, select next session
-					}
-					if(selectCourse(currSec)==true){
-						StoredItem tempVal = new StoredItem(currCourse.getCourseID(),currCourse.getCourseName(),currSec);//Simple value
-						selected.add(tempVal);
-						DebugMessager.debug("Select Course "+currCourse.getCourseID()+" "+currSec.getSectionID());
-						flag=true; // course added
-						break; // don't need to handle other section this time
-					}
-				}
-			
-				if(flag==true) continue; // skip to next course
-			}
+			DebugMessager.debug(title+"Using SimpleHandler");
+			SimpleHandler SimpleHandler = new SimpleHandler();
+			selected = SimpleHandler.run(list);
 		}else{
+			DebugMessager.debug(title+"Using ComplexHandler");
 			ComplexHandler ComplexHandler=new ComplexHandler();
 			ComplexHandler.run(list);
+			SimpleHandler SimpleHandler = new SimpleHandler();
+			selected = SimpleHandler.run(list);
 		}
 	}
 	
---
+++ src/Controller/SimpleHandler.java
@@ -0,0 +1,53 @@
+package Controller;
+
+import java.util.ArrayList;
+
+import ioMoudle.DebugMessager;
+import store.Course;
+import store.Section;
+import store.StoredItem;
+
+public class SimpleHandler {
+	static String title = "SimpleHandler : ";
+	private OverlapDetector table = new OverlapDetector();
+	private ArrayList<StoredItem> selected = new ArrayList<StoredItem>();
+	
+	public ArrayList<StoredItem> run(ArrayList<Course> list){
+		boolean flag=false;	
+		for(int i=0;i<list.size();i++){
+			flag=false; // course is not added
+			Course currCourse=list.get(i);
+			int minConflict = currCourse.getMinConflict();
+			//int SecMinConflictNum = currCourse.getSecNumMinConflict();
+			DebugMessager.debug(title+"The min Conflict of this course = "+minConflict);
+			DebugMessager.debug(title+"Handling Course "+ i+" {"+currCourse.toString()+"}");
+			
+
+			for(int j=0;j<currCourse.getSec().size();j++){
+				Section currSec = currCourse.getSec().get(j);
+				DebugMessager.debug(title+"Handling Section "+ i+" {"+currSec.toString()+"}");
+				int conflictNums = ConflictDetector.run(list, i, currSec);
+				DebugMessager.debug(title+"conflictNums = "+conflictNums);
+				if(conflictNums > list.size()/2 || (minConflict==0 && conflictNums!=minConflict)){
+					DebugMessager.debug(title+"Course Dropped");
+					continue; //bad option
+				}
+				if(selectCourse(currSec)==true){
+					StoredItem tempVal = new StoredItem(currCourse.getCourseID(),currCourse.getCourseName(),currSec);//Simple value
+					selected.add(tempVal);
+					DebugMessager.debug(title+"Select Course "+currCourse.getCourseID()+" "+currSec.getSectionID());
+					flag=true; // course added
+					break; // don't need to handle other section this time
+				}
+			}
+			if(flag==true) continue; // skip to next course
+		}
+		return selected;
+	}
+	
+	public boolean selectCourse(Section sec){		
+		//TODO : add Conflict detector here
+		return table.set(sec.getDay(),sec.getStartTime(),sec.getEndTime());
+	}
+
+}
