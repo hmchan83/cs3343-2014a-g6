cedffedce2be996579b76cf68db7950f320307e4
cheukyiksum@gmail.com <cheukyiksum@gmail.com@cabb5b36-2792-8a72-6769-e652e2edd7b8>
2014-10-16 16:31:34 +0000

    JUnit testcase directly use object rather than IO (logged by Marcus)

--- trunk/src/testscript/testscript.java
+++ trunk/src/testscript/testscript.java
@@ -1,10 +1,10 @@
 package testscript;
 
 
-import java.io.FileNotFoundException;
+
 import java.util.ArrayList;
 
-import ioModule.IO;
+
 import junit.framework.TestCase;
 
 import org.junit.Test;
@@ -18,158 +18,85 @@ import store.StoredItem;
 public class testscript extends TestCase {
 
 	@Test
-	public void testcaseA1() throws FileNotFoundException {
-		IO io = new IO();
-		ArrayList<Course> list = new ArrayList<>();
-		System.out.println("enter the number of course");
-		int courseNum = 2;
-		for(int i=0; i < courseNum;i++){
-			System.out.println("enter the id, name, no. of section of course "+(i+1));
-			if(i == 0)
-			{
-			String courseID = "CS0001";
-			String courseName = "TEST1";
-			Course currCourse = new Course(courseID,courseName);
-			int sectionNum = 2;
-			for(int j=0; j < sectionNum;j++){
-				System.out.println("enter the section (ID,day,start,end,location,CRN,credit) of course");
-				//Section(String sectionID, String day, String startTime,String endTime, String location, int cRN, int credit
-				if(j == 0)
-				{
-				Section currSec = new Section("C01","Fri","1200","1300","AC1","LT6","12345",3);
-				currCourse.addSec(currSec);
-				}
-				else
-				{
-					Section currSec = new Section("C02","Mon","1100","1200","AC1","LT8","12346",3);
-					currCourse.addSec(currSec);
-				}
-			}
-			list.add(currCourse);
-			}
-			
-			else
-			{
-		    String courseID = "CS0002";
-			String courseName = "TEST2";
-			Course currCourse = new Course(courseID,courseName);
-			int sectionNum = 1;
-			for(int j=0; j < sectionNum;j++){
-				System.out.println("enter the section (ID,day,start,end,location,CRN,credit) of course");
-				//Section(String sectionID, String day, String startTime,String endTime, String location, int cRN, int credit
-				Section currSec = new Section("C01","Fri","1200","1300","AC1","LT6","12347",3);
-				currCourse.addSec(currSec);
-				
-			}
-			list.add(currCourse);
-			}
-		}
+	public void testcaseA1()  {
+		ArrayList<Course> courselist = new ArrayList<>();
+		String courseID;
+		courseID = "CS0001";
+		String courseName;
+		courseName = "TEST1";
+		Course currCourse;
+		currCourse = new Course(courseID,courseName);
+		Section currSec;
+	    currSec = new Section("C01","Fri","1200","1300","AC1","LT6","12345",3);
+		currCourse.addSec(currSec);
+	    currSec = new Section("C02","Mon","1100","1200","AC1","LT8","12346",3);
+		currCourse.addSec(currSec);	
+		courselist.add(currCourse);
 		
-		System.out.println("Enter the min. credits that you need to take ");
+	    courseID = "CS0002";
+	    courseName = "TEST2";
+	    currCourse = new Course(courseID,courseName);
+	    currSec = new Section("C01","Fri","1200","1300","AC1","LT5","12347",3);
+		currCourse.addSec(currSec);
+		courselist.add(currCourse);
+		
 		int requireNums = 6;
 		MainController.setReqiureNums(requireNums);
 		
 		ListHandler PriorityHandler=new ListHandler();		
-		PriorityHandler.listformat(list);
+		PriorityHandler.listformat(courselist);
 		
-		io.out("\nYour Input : \n");
-		io.out(list);
 		
 		MainController MainController = new MainController();
-		MainController.run(list); // calling controller
+		MainController.run(courselist); // calling controller
 		ArrayList<StoredItem> result = MainController.result();
-		io.out("\nResult : \n");
-		int totalpriority=0;
-		if(result.isEmpty()==true){
-			System.out.println("No such result.");
-		}else{
-			for(StoredItem item : result){
-				//io.out(item.toString());
-				totalpriority+=item.getSec().getPriority();
-			}
-		}
+		
 		assertEquals(result.get(0).toString(),"12346 : CS0001 - TEST1 (C02)");
 		assertEquals(result.get(1).toString(),"12347 : CS0002 - TEST2 (C01)");
-		assertEquals(totalpriority,32);
 		}
 	
 
 	@Test
-	public void testcaseA2() throws FileNotFoundException {
-		IO io = new IO();
-		ArrayList<Course> list = new ArrayList<>();
-		System.out.println("enter the number of course");
-		int courseNum = 2;
-		for(int i=0; i < courseNum;i++){
-			System.out.println("enter the id, name, no. of section of course "+(i+1));
-			if(i == 0)
-			{
-			String courseID = "CS0001";
-			String courseName = "TEST1";
-			Course currCourse = new Course(courseID,courseName);
-			int sectionNum = 2;
-			for(int j=0; j < sectionNum;j++){
-				System.out.println("enter the section (ID,day,start,end,location,CRN,credit) of course");
-				//Section(String sectionID, String day, String startTime,String endTime, String location, int cRN, int credit
-				if(j == 0)
-				{
-				Section currSec = new Section("C01","Fri","1200","1300","AC1","LT6","12345",3);
-				currCourse.addSec(currSec);
-				}
-				else
-				{
-					Section currSec = new Section("C02","Mon","1100","1200","AC1","LT8","12346",3);
-					currCourse.addSec(currSec);
-				}
-			}
-			list.add(currCourse);
-			}
-			
-			else
-			{
-		    String courseID = "CS0002";
-			String courseName = "TEST2";
-			Course currCourse = new Course(courseID,courseName);
-			int sectionNum = 1;
-			for(int j=0; j < sectionNum;j++){
-				System.out.println("enter the section (ID,day,start,end,location,CRN,credit) of course");
-				//Section(String sectionID, String day, String startTime,String endTime, String location, int cRN, int credit
-				Section currSec = new Section("C01","Fri","1200","1300","AC1","LT6","12347",3);
-				currCourse.addSec(currSec);
-				
-			}
-			list.add(currCourse);
-			}
-		}
+	public void testcaseA2() {
+		ArrayList<Course> courselist = new ArrayList<>();
+		String courseID;
+		courseID = "CS0001";
+		String courseName;
+		courseName = "TEST1";
+		Course currCourse;
+		currCourse = new Course(courseID,courseName);
+		Section currSec;
+	    currSec = new Section("C01","Fri","1200","1300","AC1","LT6","12345",3);
+		currCourse.addSec(currSec);
+	    currSec = new Section("C02","Mon","1100","1200","AC1","LT7","12346",3);
+		currCourse.addSec(currSec);	
+		courselist.add(currCourse);
 		
-		System.out.println("Enter the min. credits that you need to take ");
+	    courseID = "CS0002";
+	    courseName = "TEST2";
+	    currCourse = new Course(courseID,courseName);
+	    currSec = new Section("C01","Fri","1200","1300","AC1","LT5","12347",3);
+		currCourse.addSec(currSec);
+		currSec = new Section("C02","Mon","1100","1200","AC1","LT8","12348",3);
+		currCourse.addSec(currSec);
+		courselist.add(currCourse);
+		
 		int requireNums = 6;
 		MainController.setReqiureNums(requireNums);
 		
 		ListHandler PriorityHandler=new ListHandler();		
-		PriorityHandler.listformat(list);
+		PriorityHandler.listformat(courselist);
 		
-		io.out("\nYour Input : \n");
-		io.out(list);
 		
 		MainController MainController = new MainController();
-		MainController.run(list); // calling controller
+		MainController.run(courselist); // calling controller
 		ArrayList<StoredItem> result = MainController.result();
-		io.out("\nResult : \n");
-		int totalpriority=0;
-		if(result.isEmpty()==true){
-			System.out.println("No such result.");
-		}else{
-			for(StoredItem item : result){
-				//io.out(item.toString());
-				totalpriority+=item.getSec().getPriority();
-			}
-		}
-		assertEquals(result.get(0).toString(),"12346 : CS0001 - TEST1 (C02)");
-		assertEquals(result.get(1).toString(),"12347 : CS0002 - TEST2 (C01)");
-		assertEquals(totalpriority,32);
-		}
+		
+		assertEquals(result.get(0).toString(),"12346 : CS0001 - TEST1 (C01)");//problem come here!
+		assertEquals(result.get(1).toString(),"12347 : CS0002 - TEST2 (C02)");
+		//
 	}
+}
 	
 
 
